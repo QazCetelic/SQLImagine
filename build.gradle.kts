388f78d74310e432d0a937b61d179972d0138bee
QazCetelic <qaz.cetelic@protonmail.com>
2022-03-13 14:27:17 +0100

    Add gradle

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.0"
+}
+
+group = "me.qaz"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(files(
+        // Get the jar at https://github.com/QazCetelic/Neatlin/releases
+        "$projectDir/../../Kotlin/Neatlin/build/libs/Neatlin-jvm-0.3.3.jar",
+        // Get the jar at https://github.com/QazCetelic/Kwing/releases
+        "$projectDir/../../JSwing/Kwing/build/libs/Kwing-0.1.0.jar"
+    ))
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
