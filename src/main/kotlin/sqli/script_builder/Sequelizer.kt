2d5396f6d8083de75c26021a7af3a8dd775227e3
QazCetelic <qaz.cetelic@protonmail.com>
2022-03-13 14:36:34 +0100

    Add SQL generator

---
+++ src/main/kotlin/sqli/script_builder/Sequelizer.kt
@@ -0,0 +1,76 @@
+package sqli.script_builder
+
+import sqli.parser.Attribute
+import sqli.parser.ParseInstance
+import sqli.parser.Table
+import sqli.parser.Type
+
+/**
+ * Turns parsed tables into an SQL script.
+ */
+class Sequelizer {
+
+    private val indent: String
+    private val database: String
+
+    constructor(database: String, indent: String = "  ") {
+        this.database = database
+        if (indent.isNotBlank()) throw IllegalArgumentException("Indent must only contain whitespace")
+        this.indent = indent
+    }
+
+    fun sequelize(parsed: ParseInstance): String {
+        return parsed.tables.joinToString(separator = "\n") { sequelizeTable(it) }
+    }
+
+    private fun sequelizeTable(table: Table): String {
+        val attributes = table.attributes.entries.map { it.toPair() }
+        val dbName = "${database}.${table.name}"
+        return buildString {
+            append("-- Removes the old table if it exists")
+            append('\n')
+            append("DROP TABLE IF EXISTS $dbName;")
+            append('\n')
+            append("CREATE TABLE $dbName (")
+            append('\n')
+            append(sequelizeAttributes(attributes))
+            append('\n')
+            append(");")
+        }
+    }
+
+    private fun sequelizeAttributes(attributes: List<Pair<String, Attribute>>): String {
+        val fields = attributes.joinToString("\n") {
+            sequelizeAttribute(it).prependIndent(indent) + ","
+        }
+        val foreignKeys = attributes.filter { it.second.references.isNotEmpty() }.joinToString("\n") {
+            val (name, attribute) = it
+            val reference = attribute.references.first()
+            "FOREIGN KEY (${name}) REFERENCES ${reference.first}(${reference.second})".prependIndent(indent) + ","
+        }
+        return "$fields\n$foreignKeys".trimEnd().removeSuffix(",")
+    }
+
+    private fun sequelizeAttribute(namedAttribute: Pair<String, Attribute>): String {
+        val (name, attribute) = namedAttribute
+        return buildString {
+            append(name)
+            append(" ")
+            append(sequelizeType(attribute.type))
+            if (attribute.primaryKey) append(" PRIMARY KEY")
+            if (attribute.nullable) append(" NULL")
+        }
+    }
+
+    private fun sequelizeType(type: Type): String {
+        return when (type) {
+            Type.Num -> "INTEGER"
+            Type.Dec -> "FLOAT"
+            Type.Str -> "VARCHAR(64)"
+            Type.Bool -> "TINYINT(1)"
+            Type.Date -> "DATE"
+            else -> TODO("Type not implemented yet")
+        }
+    }
+
+}
